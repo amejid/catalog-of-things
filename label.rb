ce4a42034364342b7f49613fba9d06aa45a749dd
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-10-10 17:18:35 +0300

    Create label class

---
+++ label.rb
@@ -0,0 +1,13 @@
+class Label
+  def initialize(id, title, color)
+    @id = id || rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
+end
