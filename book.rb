f6b940c34f919bf85e37ff6c731b230f9fb0321d
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-10-10 17:18:18 +0300

    Create book class

---
+++ book.rb
@@ -0,0 +1,15 @@
+require_relative 'item'
+
+class Book < Item
+  def initialize(id, publish_date, publisher, cover_state)
+    super(id, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
