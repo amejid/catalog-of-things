5b1c07573689d3dc60a8c63852ff12f69f4452f1
sireclifford <dev.sire.clifford@gmail.com>
2022-10-10 17:56:07 +0000

    add attr_readers

--- game.rb
+++ game.rb
@@ -1,6 +1,8 @@
 require_relative 'item'
 
 class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
   def initialize(id, publish_date, multiplayer, last_played_at)
     super(id, publish_date)
     @multiplayer = multiplayer
