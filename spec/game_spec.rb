dd6a672136e428477ca3998a924ae5b72b6fd30a
sireclifford <dev.sire.clifford@gmail.com>
2022-10-11 19:46:16 +0000

    fix linter

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,25 +1,25 @@
 require_relative '../game'
 
 describe Game do
-    context 'With valid arguments' do
-        it 'should return string id' do
-        game = Game.new(nil, '2022-02-02', 'Yes', '2020-02-02')
-        expect(game.id.class).to eq String
-        end
-
-        it 'should return publish date' do
-        game = Game.new(nil, '2022-02-11', 'No', '2020-02-02')
-        expect(game.publish_date).to eq '2022-02-11'
-        end
-
-        it 'should return multiplayer' do
-        game = Game.new(nil, '2022-02-11', 'No', '2020-02-02')
-        expect(game.multiplayer).to eq 'No'
-        end
-
-        it 'should return last played at' do
-        game = Game.new(nil, '2022-02-11', 'No', '2020-02-02')
-        expect(game.last_played_at).to eq '2020-02-02'
-        end
+  context 'With valid arguments' do
+    it 'should return string id' do
+      game = Game.new(nil, '2022-02-02', 'Yes', '2020-02-02')
+      expect(game.id.class).to eq String
     end
+
+    it 'should return publish date' do
+      game = Game.new(nil, '2022-02-11', 'No', '2020-02-02')
+      expect(game.publish_date).to eq '2022-02-11'
+    end
+
+    it 'should return multiplayer' do
+      game = Game.new(nil, '2022-02-11', 'No', '2020-02-02')
+      expect(game.multiplayer).to eq 'No'
+    end
+
+    it 'should return last played at' do
+      game = Game.new(nil, '2022-02-11', 'No', '2020-02-02')
+      expect(game.last_played_at).to eq '2020-02-02'
+    end
+  end
 end
