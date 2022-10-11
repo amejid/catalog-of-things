172cb6c00f826f22192a8e6fd84e9941d5dd5d57
sireclifford <dev.sire.clifford@gmail.com>
2022-10-11 19:39:14 +0000

    write test for author and game class

---
+++ spec/author_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../author'
+
+describe Author do
+    context 'With valid arguments' do
+        it 'should return string id' do
+        author = Author.new(nil, 'Isaac', 'Asimov')
+        expect(author.id.class).to eq String
+        end
+    
+        it 'should return first name' do
+        author = Author.new(nil, 'Isaac', 'Asimov')
+        expect(author.first_name).to eq 'Isaac'
+        end
+    
+        it 'should return last name' do
+        author = Author.new(nil, 'Isaac', 'Asimov')
+        expect(author.last_name).to eq 'Asimov'
+        end
+    end
+end
