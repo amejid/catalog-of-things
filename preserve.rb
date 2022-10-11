a481242b4c24ae4a9b53e5a601a61fa3e6cf70a2
sireclifford <dev.sire.clifford@gmail.com>
2022-10-11 09:36:08 +0000

    create methods to load & save game and author

--- preserve.rb
+++ preserve.rb
@@ -37,9 +37,25 @@ class Preserve
     end
   end
 
-  def load_games; end
+  def load_games
+    return unless File.exist?('./data/games.json')
 
-  def load_authors; end
+    games_loaded = JSON.parse(File.read('./data/games.json'))
+    games_loaded.each do |game|
+      new_game = Game.new(game['id'], game['publish_date'], game['multiplayer'], game['last_played_at'])
+      @games << new_game
+    end
+  end
+
+  def load_authors
+    return unless File.exist?('./data/authors.json')
+      
+      authors_loaded = JSON.parse(File.read('./data/authors.json'))
+      authors_loaded.each do |author|
+        new_author = Author.new(author['id'], author['first_name'], author['last_name'])
+        @authors << new_author
+      end
+  end
 
   def load_music_albums; end
 
@@ -68,9 +84,28 @@ class Preserve
     end
   end
 
-  def save_game; end
+  def save_game(game)
+    new_game = { id: game.id, publish_date: game.publish_date, multiplayer: game.multiplayer,
+                 last_played_at: game.last_played_at }
+    if File.exist?('./data/games.json')
+      games_loaded = JSON.parse(File.read('./data/games.json'))
+      games_loaded << new_game
+      File.write('./data/games.json', JSON.pretty_generate(games_loaded))
+    else
+      File.write('./data/games.json', JSON.pretty_generate([new_game]))
+    end
+  end
 
-  def save_author; end
+  def save_author(author)
+   new_author = { id: author.id, first_name: author.first_name, last_name: author.last_name }
+    if File.exist?('./data/authors.json')
+      authors_loaded = JSON.parse(File.read('./data/authors.json'))
+      authors_loaded << new_author
+      File.write('./data/authors.json', JSON.pretty_generate(authors_loaded))
+    else
+      File.write('./data/authors.json', JSON.pretty_generate([new_author]))
+    end
+  end
 
   def save_music_album; end
 
