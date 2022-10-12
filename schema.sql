dc647fe0c0e7894e5073f0d51e04f6375d942a36
Roseokpe <roseokpe@gmail.com>
2022-10-12 10:57:30 -0700

    add  music album and genre schema

--- schema.sql
+++ schema.sql
@@ -14,3 +14,18 @@ CREATE TABLE book
     cover_state  VARCHAR(50),
     label_id     INTEGER REFERENCES label (id)
 )
+
+CREATE TABLE genre
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+)
+
+CREATE TABLE music_album
+(
+    id           SERIAL PRIMARY KEY,
+    publish_date DATE,
+    archived     BOOLEAN,
+    on_spotify   BOOLEAN,
+    genre_id     INTEGER REFERENCES genre (id)
+)
